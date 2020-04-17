0f691c93836c44a2901e6f8398c646ac4dadd723
Sergey Martynov <smartynov@MacBook-Pro-Sergey.local>
2020-04-17 10:51:50 +0300

    add tests for set

--- conftest.py
+++ conftest.py
@@ -8,3 +8,13 @@ def first_fixture():
 def fixture_return_numbers():
     list_03 = [10, 20, 30]
     return list_03
+
+@pytest.fixture
+def fixtures_set_return_numbers():
+    set_01 = {10, 20, 30}
+    return set_01
+
+@pytest.fixture
+def fixtures_set_return_words():
+    set_01 = {'rock', 'post-rock', 'punk-rock'}
+    return set_01
